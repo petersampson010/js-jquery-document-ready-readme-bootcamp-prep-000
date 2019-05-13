24d4f5c1142b03a4d3dce9b07adf6c7a153e8324
Peter Sampson <petersampson010@gmail.com>
2019-05-13 19:58:57 +0000

    Automatically backed up by Learn

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
 $(document).ready(function() {
-<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/3.0.0/jquery.min.js"><script>
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/3.0.0/jquery.min.js"></script>
 $("#text").append("this is so freaking cool.");
 }
